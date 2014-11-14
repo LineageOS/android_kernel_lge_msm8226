46cac5b62dbc3d88520cb51028e4ad74a6edc130
Quarx2k <agent00791@gmail.com>
2014-11-14 20:23:04 +0600

    Fix compile nfc
    Change-Id: Icd412529569b56c7a1598b1cb139cc465ebdde4a

--- include/linux/nfc/pn544.h
+++ include/linux/nfc/pn544.h
@@ -25,6 +25,18 @@
 
 #include <linux/i2c.h>
 
+#define PN544_MAGIC	0xE9
+
+/*
+ * PN544 power control via ioctl
+ * PN544_SET_PWR(0): power off
+ * PN544_SET_PWR(1): power on
+ * PN544_SET_PWR(2): reset and power on with firmware download enabled
+ */
+#define PN544_SET_PWR	_IOW(PN544_MAGIC, 0x01, unsigned int)
+
+#define PN544_HW_REVISION _IOR(PN544_MAGIC, 0x02, unsigned int)
+
 #define PN544_DRIVER_NAME	"pn544"
 #define PN544_MAXWINDOW_SIZE	7
 #define PN544_WINDOW_SIZE	4
