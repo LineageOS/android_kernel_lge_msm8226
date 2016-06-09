2acf213bd25f7e4e54bfed0038efa96809fbb2c7
kn06497 <kn06497@hotmail.com>
2016-06-09 18:10:25 -0500

    jagnm: Fix camera 13Mpx
    Tnks @Nothing-Dev
    
    Change-Id: I76bb52d2473aba79692311cf161f9d6388518587
    Signed-off-by: kn06497 <kn06497@hotmail.com>

--- arch/arm/mach-msm/clock-8226.c
+++ arch/arm/mach-msm/clock-8226.c
@@ -3446,6 +3446,9 @@ static struct clk_lookup msm_clocks_8226[] = {
 //	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "90.qcom,camera"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6d.qcom,camera"),
 //FIX_ME JB_MR2
+#ifdef CONFIG_IMX091
+	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "20.qcom,camera_imx091"),
+#endif
 #ifdef CONFIG_IMX111	/* LGE_CHANGE,  Added for IMX111 clock source */
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "20.qcom,camera_rev_0"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "20.qcom,camera_rev_a"),
@@ -3526,6 +3529,9 @@ static struct clk_lookup msm_clocks_8226[] = {
 	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "6f.qcom,camera"),
 //	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "90.qcom,camera"),
 	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "6d.qcom,camera"),
+#ifdef CONFIG_IMX091
+	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "20.qcom,camera_imx091"),
+#endif
 #ifdef CONFIG_IMX111	/* LGE_CHANGE,  Added for IMX111 clock source */
 	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "20.qcom,camera_rev_0"),
 	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "20.qcom,camera_rev_a"),
